a88232e3cb36f6caf61172a63fb03c1f1ab37afc
ucaszx <ucaszx@gmail.com>
2017-09-24 09:33:04 +0000

    add chinese tokenizer

---
+++ drqa/tokenizers/ltp_tokenizer.py
@@ -0,0 +1,70 @@
+
+import unicodedata
+import copy
+import json
+import pexpect
+import re
+from os import path
+from pyltp import Segmentor
+from pyltp import Postagger
+from pyltp import NamedEntityRecognizer
+
+from .tokenizer import Tokens, Tokenizer
+
+class LTPTokenizer(Tokenizer):
+
+    def __init__(self, **kwargs):
+        self.annotators = copy.deepcopy(kwargs.get('annotators', set()))
+        self.data_dir = '/var/yr/zhouxiang/data/ltp_data'
+        #print(self.data_dir)
+        self.segmentor = Segmentor()
+        self.segmentor.load(path.join(self.data_dir, 'cws.model'))
+        self.postagger = Postagger()
+        self.postagger.load(path.join(self.data_dir, 'pos.model'))
+        self.ne_recognizer = NamedEntityRecognizer()
+        self.ne_recognizer.load(path.join(self.data_dir, 'ner.model'))
+
+    #def __del__(self):
+    #    self.segmentor.release()
+    #    self.postagger.release()
+    #    self.ne_recognizer.release()
+
+    def tokenize(self, text):
+        normal_text = unicodedata.normalize('NFKC', text).replace('\\', ' ')
+        #print(normal_text)
+        normal_text = re.sub(r'[ \n\t]+', normal_text, ' ').strip()
+        if len(normal_text) == 0: print('empty line')
+        clean_text = normal_text.split(' ')
+        words = [word for split in clean_text for word in list(self.segmentor.segment(split))]
+        #print(words)
+        postags = list(self.postagger.postag(words))
+        #print(postags)
+        netags = list(self.ne_recognizer.recognize(words, postags))
+        #print(netags)
+        spans = []
+        lemmas = []
+        offset = 0
+        for word in words:
+            lemmas.append(word)
+            word_start = normal_text.find(word, offset)
+            if word_start == -1:
+                raise RuntimeError('%s not found in\n%s\n[%d:]' % (word, normal_text, offset))
+            word_end = word_start + len(word)
+            spans.append((word_start, word_end))
+            offset = word_end
+        text_ws = []
+        for i in range(len(spans)):
+            start_ws = spans[i][0]
+            end_ws = spans[i][1] if i+1 >= len(spans) else spans[i+1][0]
+            text_ws.append(normal_text[start_ws: end_ws])
+        data = []
+        for tup in zip(words, text_ws, spans, postags, lemmas, netags):
+            data.append(tup)
+        return Tokens(data, self.annotators)
+
+if __name__ == '__main__':
+    tokenizer = LTPTokenizer(**{'annotators': {'lemma', 'pos', 'ner'}})
+    data = tokenizer.tokenize('元芳你怎么看？ 我趴窗户上看')
+    for item in data:
+        print(item)
+
---
+++ scripts/reader/sogou_preprocess.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# Copyright 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+"""Preprocess the SQuAD dataset for training."""
+
+import argparse
+import os
+import sys
+import json
+import time
+
+from multiprocessing import Pool
+from multiprocessing.util import Finalize
+from functools import partial
+from drqa import tokenizers
+
+# ------------------------------------------------------------------------------
+# Tokenize + annotate.
+# ------------------------------------------------------------------------------
+
+TOK = None
+
+
+def init(tokenizer_class, options):
+    global TOK
+    TOK = tokenizer_class(**options)
+    Finalize(TOK, TOK.shutdown, exitpriority=100)
+
+
+def tokenize(text):
+    """Call the global process tokenizer on the input text."""
+    global TOK
+    tokens = TOK.tokenize(text)
+    output = {
+        'words': tokens.words(),
+        'offsets': tokens.offsets(),
+        'pos': tokens.pos(),
+        'lemma': tokens.lemmas(),
+        'ner': tokens.entities(),
+    }
+    return output
+
+
+# ------------------------------------------------------------------------------
+# Process dataset examples
+# ------------------------------------------------------------------------------
+
+
+def load_dataset(path):
+    """Load json file and store fields separately."""
+    output = {'qids': [], 'questions': [], 'answers': [],
+              'contexts': [], 'qid2cid': []}
+
+    with open(path) as f:
+        for line in f:
+            qaid = 100001
+            item = json.loads(line)
+            if len(item['query']) == 0 or len(item['answer']) == 0: continue
+            for passage in item['passages']:
+                if len(passage) == 0: continue
+                output['contexts'].append(passage['passage_text'])
+                output['qids'].append(str(qaid))
+                output['questions'].append(item['query'])
+                output['qid2cid'].append(len(output['contexts']) - 1)
+                output['answers'].append(item['answer'])
+                qaid += 1
+    print(len(output['questions']),len(output['contexts']))
+    return output
+
+
+def find_answer(offsets, begin_offset, end_offset):
+    """Match token offsets with the char begin/end offsets of the answer."""
+    start = [i for i, tok in enumerate(offsets) if tok[0] == begin_offset]
+    end = [i for i, tok in enumerate(offsets) if tok[1] == end_offset]
+    assert(len(start) <= 1)
+    assert(len(end) <= 1)
+    if len(start) == 1 and len(end) == 1:
+        return start[0], end[0]
+
+
+def process_dataset(data, tokenizer, workers=None):
+    """Iterate processing (tokenize, parse, etc) dataset multithreaded."""
+    tokenizer_class = tokenizers.get_class(tokenizer)
+    init(tokenizer_class,  {'annotators': {'lemma'}})
+    #make_pool = partial(Pool, workers, initializer=init)
+    #workers1 = make_pool(initargs=(tokenizer_class, {'annotators': {'lemma'}}))
+    #q_tokens = workers1.map(tokenize, data['questions'])
+    #workers1.close()
+    #workers1.join()
+    print("tokenizing questions and contexts ...")
+    q_tokens = []
+    c_tokens = []
+    for idx in range(len(data['questions'])):
+        question = data['questions'][idx]
+        context = data['contexts'][idx]
+        if len(question) == 0 or len(context) == 0:
+          continue
+        print(type(tokenize(question)))
+        print(type(tokenize(context)))
+        q_tokens.append(tokenize(question))
+        c_tokens.append(tokenize(context))
+
+    #workers2 = make_pool(
+    #    initargs=(tokenizer_class, {'annotators': {'lemma', 'pos', 'ner'}})
+    #)
+    #c_tokens = workers2.map(tokenize, data['contexts'])
+    #workers2.close()
+    #workers2.join()
+    assert(len(q_tokens) == len(c_tokens))
+    for idx in range(len(q_tokens)):
+        question = q_tokens[idx]['words']
+        qlemma =   q_tokens[idx]['lemma']
+        document = c_tokens[data['qid2cid'][idx]]['words']
+        offsets =  c_tokens[data['qid2cid'][idx]]['offsets']
+        lemma = c_tokens[data['qid2cid'][idx]]['lemma']
+        pos =   c_tokens[data['qid2cid'][idx]]['pos']
+        ner =   c_tokens[data['qid2cid'][idx]]['ner']
+        ans_tokens = []
+        ans = data['answers'][idx]  # answer the text
+        ans_start = data['contexts'][idx].find(ans)
+        ans_end = ans_start + len(ans)
+        found = find_answer(offsets, ans_start, ans_end)
+        if found:
+            ans_tokens.append(found)
+            yield {
+              'id': data['qids'][idx],
+              'question': question,
+              'document': document,
+              'offsets': offsets,
+              'answers': ans_tokens,
+              'qlemma': qlemma,
+              'lemma': lemma,
+              'pos': pos,
+              'ner': ner,
+            }
+
+
+# -----------------------------------------------------------------------------
+# Commandline options
+# -----------------------------------------------------------------------------
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', type=str, help='Path to sogouqa data directory',
+                    default='/DrQA/data/sogouqa')
+parser.add_argument('--out_dir', type=str, help='Path to output file dir',
+                    default='/DrQA/data/sogouqa')
+parser.add_argument('--split', type=str, help='Filename for train/dev split',
+                    default='train_factoid_1')
+                    #default='examples')
+parser.add_argument('--workers', type=int, default=1)
+parser.add_argument('--tokenizer', type=str, default='ltp')
+args = parser.parse_args()
+
+t0 = time.time()
+
+in_file = os.path.join(args.data_dir, args.split + '.json')
+print('Loading dataset %s' % in_file, file=sys.stderr)
+dataset = load_dataset(in_file)
+
+out_file = os.path.join(
+    args.out_dir, '%s-processed-%s.txt' % (args.split, args.tokenizer)
+)
+print('Will write to file %s' % out_file, file=sys.stderr)
+with open(out_file, 'w') as f:
+    for ex in process_dataset(dataset, args.tokenizer, args.workers):
+        f.write(json.dumps(ex, ensure_ascii=False) + '\n')
+print('Total time: %.4f (s)' % (time.time() - t0))
